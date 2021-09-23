9e08b5c9a6f70731233f2dbf25def79406825156
Stepan-jpg <stepan.murathanov@mail.ru>
2021-09-23 22:30:15 +0400

    'new_commit_backend'

--- social_network/channel/admin.py
+++ social_network/channel/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from channel.models import Channel, Post, ImagePost, Comment
+from channel.models import Channel, Post, ImagePost
 
 admin.site.register(Channel)
 admin.site.register(Post)
