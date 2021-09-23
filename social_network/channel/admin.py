f0b50359785b6278c94c8b0a2b9787526437288d
maestrolix <stepanmurathanov@gmail.com>
2021-09-24 00:44:00 +0400

    'refactoring_models'

--- social_network/channel/admin.py
+++ social_network/channel/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from channel.models import Channel, Post, ImagePost
+from channel.models import Channel, Post, ImagePost, Comment
 
 admin.site.register(Channel)
 admin.site.register(Post)
